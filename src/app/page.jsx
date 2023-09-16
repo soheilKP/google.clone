9727102e85293f3fa749e1273384338c01a1b017
soheilKP <soheilkarimi79@gmail.com>
2023-09-16 23:44:25 +0330

    Create the body section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,25 @@
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
 
 const Home = () => {
   return <>
+    {/* Header */}
     <HomeHeader />
+
+    {/* Body */}
+    <div className="flex flex-col items-center mt-24">
+      <Image 
+      width='300'
+      height='100'
+      src="google-ar21.svg"
+      alt="google-logo"
+      />
+      <HomeSearch />
+    </div>
+
+    {/* Footer */}
+
   </>
 };
 
---
+++ src/components/HomeSearch.jsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
+
+const HomeSearch = () => {
+  const [input, setInput] = useState("");
+  const router = useRouter();
+  const [randomSearchLoading, setRandomSearchLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search?searchTerm=${input}`);
+  }
+
+  async function randomSearch() {
+    setRandomSearchLoading(true);
+    const random = await fetch("https://random-word-api.herokuapp.com/word")
+      .then((res) => res.json())
+      .then((data) => data[0]);
+    if (!random) return;
+    router.push(`/search/web?searchTerm=${random}`);
+    setRandomSearchLoading(false);
+  }
+
+  return (
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 px-5 py-3 rounded-full hover:shadow-md transition-shadow focus-within:shadow-md sm:max-w-xl lg:max-w-2xl"
+      >
+        <AiOutlineSearch className="text-xl text-gray-500 mr-3" />
+        <input
+          type="text"
+          className="flex-grow focus:outline-none "
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+        />
+        <BsFillMicFill className="text-lg" />
+      </form>
+      <div className="flex flex-col space-y-2 sm:space-y-0 sm:flex-row sm:space-x-4 justify-center mt-8">
+        <button onClick={handleSubmit} className="btn">
+          Google Search
+        </button>
+        <button
+          disabled={randomSearchLoading}
+          onClick={randomSearch}
+          className="btn flex items-center justify-center disabled:opacity-50"
+        >
+          {randomSearchLoading ? (
+            <img
+              src="spinner.svg"
+              alt="spinner"
+              className="w-4 h-6 text-center"
+            />
+          ) : (
+            "I am Feeling Lucky"
+          )}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default HomeSearch;
