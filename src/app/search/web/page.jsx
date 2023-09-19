20be96b474127868478cfd4712f43941ae3cef6b
soheilKP <soheilkarimi79@gmail.com>
2023-09-19 11:19:53 +0330

    fetch data from google search

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,16 @@
-const WebSearchPage = () => {
-  return <div>WebSearchPage</div>;
+const WebSearchPage = async ({ searchParams }) => {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const results = data.items;
+
+  return (
+    <>
+      {results &&
+        results.map((result) => <h1 key={result.id}>{result.title}</h1>)}
+    </>
+  );
 };
 
 export default WebSearchPage;
