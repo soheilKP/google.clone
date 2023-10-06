c47a7f443da87428ac78fa8a61761e5948754f5f
soheilKP <soheilkarimi79@gmail.com>
2023-10-06 11:27:29 +0330

    Update npm package

--- src/components/PaginationButtons.jsx
+++ src/components/PaginationButtons.jsx
@@ -9,28 +9,34 @@ const PaginationButtons = () => {
   const searchTerm = searchParams.get("searchTerm");
   const startIndex = Number(searchParams.get("start")) || 1;
 
+  const previousLink =
+    startIndex > 10 ? (
+      <Link
+        href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex - 10}`}
+      >
+        <div className="flex flex-col cursor-pointer items-center hover:underline">
+          <FaCircleChevronLeft className="h-5" />
+          <p>Previous</p>
+        </div>
+      </Link>
+    ) : null;
+
+  const nextLink =
+    startIndex < 100 ? (
+      <Link
+        href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex + 10}`}
+      >
+        <div className="flex flex-col cursor-pointer items-center hover:underline">
+          <FaCircleChevronRight className="h-5" />
+          <p>Next</p>
+        </div>
+      </Link>
+    ) : null;
+
   return (
     <div className="text-blue-500 flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm:px-0">
-      {startIndex > 10 && (
-        <Link
-          href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex - 10}`}
-        >
-          <div className="flex flex-col cursor-pointer items-center hover:underline">
-            <FaCircleChevronLeft className="h-5" />
-            <p>Previous</p>
-          </div>
-        </Link>
-      )}
-      {startIndex < 100 && (
-        <Link
-          href={`${pathname}?searchTerm=${searchTerm}&start=${startIndex + 10}`}
-        >
-          <div className="flex flex-col cursor-pointer items-center hover:underline">
-            <FaCircleChevronRight className="h-5" />
-            <p>Next</p>
-          </div>
-        </Link>
-      )}
+      {previousLink}
+      {nextLink}
     </div>
   );
 };
